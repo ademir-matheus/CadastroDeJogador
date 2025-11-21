9fb463de7074a183104a4058fbc8dad372df83cd
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-11-21 00:32:47 -0300

    first commit

---
+++ src/main/java/br/com/rimeda/CadastroDeJogador/CadastroDeJogadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.rimeda.CadastroDeJogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeJogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeJogadorApplication.class, args);
+	}
+
+}
