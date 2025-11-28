193c08b0935fe7661b99afa64c23f2a85cdd1dfc
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-11-28 00:31:24 -0300

    feat: Cria a entidade JogadorModel

--- src/main/java/br/com/rimeda/CadastroDeJogador/JogadorController.java
+++ src/main/java/br/com/rimeda/CadastroDeJogador/JogadorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class JogadorController {
 
     // Cria primeira rota;
     @GetMapping("/boasVindas")
