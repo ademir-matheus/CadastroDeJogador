cccf118edde7ceba7111aaeffa52f99892242b8b
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-12-22 22:30:40 -0300

    chore: inclui dto e mapper do clube.

--- src/main/java/br/com/rimeda/CadastroDeJogador/Clube/ClubeMapper.java
+++ src/main/java/br/com/rimeda/CadastroDeJogador/Clube/ClubeMapper.java
@@ -1,4 +1,22 @@
 package br.com.rimeda.CadastroDeJogador.Clube;
 
 public class ClubeMapper {
+
+    public ClubeModel map (ClubeDTO clubeDTO){
+        ClubeModel clubeModel = new ClubeModel();
+        clubeModel.setId(clubeDTO.getId());
+        clubeModel.setNome(clubeDTO.getNome());
+        clubeModel.setJogadores(clubeDTO.getJogadores());
+
+        return clubeModel;
+    }
+
+    public ClubeDTO map (ClubeModel clubeModel) {
+        ClubeDTO clubeDTO = new ClubeDTO();
+        clubeDTO.setId(clubeModel.getId());
+        clubeDTO.setNome(clubeModel.getNome());
+        clubeDTO.setJogadores(clubeModel.getJogadores());
+        return clubeDTO;
+    }
+
 }
