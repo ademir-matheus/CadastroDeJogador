a958b35e9046a7a44874c25f85361ed8ae75f5bd
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-12-22 22:23:19 -0300

    chore: inclui dto para entidade e cria mapper

--- src/main/java/br/com/rimeda/CadastroDeJogador/Jogador/JogadorDTO.java
+++ src/main/java/br/com/rimeda/CadastroDeJogador/Jogador/JogadorDTO.java
@@ -1,4 +1,18 @@
 package br.com.rimeda.CadastroDeJogador.Jogador;
 
+import br.com.rimeda.CadastroDeJogador.Clube.ClubeModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JogadorDTO {
+    private Long id;
+    private String nome;
+    private String instagram;
+    private int idade;
+    private ClubeModel clube;
+    private String corDePele;
 }
--- src/main/java/br/com/rimeda/CadastroDeJogador/Jogador/JogadorMapper.java
+++ src/main/java/br/com/rimeda/CadastroDeJogador/Jogador/JogadorMapper.java
@@ -1,4 +1,34 @@
 package br.com.rimeda.CadastroDeJogador.Jogador;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JogadorMapper {
+
+    public JogadorModel map (JogadorDTO jogadorDTO) {
+
+        JogadorModel jogadorModel = new JogadorModel();
+        jogadorModel.setId(jogadorDTO.getId());
+        jogadorModel.setNome(jogadorDTO.getNome());
+        jogadorModel.setIdade(jogadorDTO.getIdade());
+        jogadorModel.setInstagram(jogadorDTO.getInstagram());
+        jogadorModel.setClube(jogadorDTO.getClube());
+        jogadorModel.setCorDePele(jogadorDTO.getCorDePele());
+
+        return jogadorModel;
+    }
+
+    public JogadorDTO map (JogadorModel jogadorModel){
+
+        JogadorDTO jogadorDTO = new JogadorDTO();
+        jogadorDTO.setId(jogadorModel.getId());
+        jogadorDTO.setNome(jogadorModel.getNome());
+        jogadorDTO.setIdade(jogadorModel.getIdade());
+        jogadorDTO.setInstagram(jogadorModel.getInstagram());
+        jogadorDTO.setClube(jogadorModel.getClube());
+        jogadorDTO.setCorDePele(jogadorModel.getCorDePele());
+        return jogadorDTO;
+    }
+
+
 }
